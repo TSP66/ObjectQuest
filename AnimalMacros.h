fad79ebb2b166d897be8e8bbe64fbac6624cef6f
TSP66 <62830753+TSP66@users.noreply.github.com>
2024-10-07 13:11:13 +1030

    Added Fish

--- AnimalMacros.h
+++ AnimalMacros.h
@@ -4,6 +4,8 @@
 #include "AquaticAnimal.h"
 #include "LandAnimal.h"
 
+//To add an another animal modify this section
+
 #define LAND_ANIMAL_LIST(XX) \
     XX(Lion) \
     XX(Tiger) 
@@ -11,21 +13,27 @@
 #define AQUATIC_ANIMAL_LIST(XX) \
     XX(Dolphin) \
     XX(Otter) \
+    XX(Fish)
 
 #define CAPITALIZE_Otter OTTER
 #define CAPITALIZE_Lion LION
 #define CAPITALIZE_Tiger TIGER
 #define CAPITALIZE_Dolphin DOLPHIN
+#define CAPITALIZE_Fish FISH
 
 #define PRICE_Lion 30
 #define PRICE_Tiger 50
 #define PRICE_Dolphin 20
 #define PRICE_Otter 20
+#define PRICE_Fish 10
 
 #define LIFESPAN_Lion 10.0
 #define LIFESPAN_Tiger 5.0
 #define LIFESPAN_Dolphin 7.0
 #define LIFESPAN_Otter 2.0
+#define LIFESPAN_Fish 1.0
+
+//------------------------------------------
 
 #define CAPITALIZE_ANIMAL(animal) CAPITALIZE_##animal
 #define PRICE_ANIMAL(animal) PRICE_##animal
