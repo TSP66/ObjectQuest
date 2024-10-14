287786e056b28b5a039239ef4aaa7b8a0fd301c6
Jack Anderson <a1852156@adelaide.edu.au>
2024-10-14 15:24:48 +1030

    Jack

--- Animal.cpp
+++ Animal.cpp
@@ -1,24 +1,29 @@
 #include "Animal.h"
-
+//animal class. Used to specify animal features
 Animal::Animal(int id, std::string name, int lifespan) : lifespan(lifespan), id(id), name(name){
     Animal::cost = 50;
     Animal::happiness = 100.0;
     Animal::sex = (Sex) (rand() > RAND_MAX/2);
 }
-
+//get lifespan used to check the lifespan of animals
 float Animal::get_lifespan(){
     return Animal::lifespan;
 }
+//used to set an animals lifespan
 void Animal::set_lifespan(float lifespan){
     Animal::lifespan = lifespan;
 }
+//used to observe an animals current age
 float Animal::get_age(){
     return Animal::age;
 }
+//used to set an animals age
 void Animal::set_age(float age){
     Animal::age = age;
 }
-
+//used to observe the animals ID. Each animal receives an ID when created
+//This is important for locating specific animals and all their 
+//specifications such as age lifespan and happiness
 int Animal::get_id(){
     return Animal::id;
 }
@@ -27,18 +32,21 @@ Death Animal::timestep(){
     return NONE;
 }
 
+// used to find animals name 
 std::string Animal::get_name(){
     return Animal::name;
 }
-
+//comand used to observe an animals cost 
 int Animal::get_cost(){
     return Animal::cost;
 }
 
+//used to determine an animals 
 Sex Animal::get_sex(){
     return Animal::sex;
 }
-
+//return animal sex in a string. Ensures user has a way of 
+//observing gender while playing 
 std::string sexToString(Sex sex){
     switch (sex){
         case MALE:
@@ -48,6 +56,8 @@ std::string sexToString(Sex sex){
     }
 }
 
+//returns how the animal died
+//returns a string to the user on the current animals life status
 std::string deathToString(Death death){
     switch (death){
         case ILLNESS:
@@ -61,13 +71,15 @@ std::string deathToString(Death death){
     }
 }
 
+// returns the specific animals hunger 
 float Animal::get_hunger(void){
     return Animal::hunger;
 }
+//returns the animals happiness 
 float Animal::get_happiness(void){
     return Animal::happiness;
 }
-
+//sets animals happiness 
 void Animal::set_happiness(float happiness){
     Animal::happiness = happiness;
 }
@@ -75,6 +87,7 @@ void Animal::set_happiness(float happiness){
 void Animal::set_hunger(float hunger){
     Animal::hunger = hunger;
 }
+//sets the cost of animals
 
 void Animal::set_cost(int cost){
     Animal::cost = cost;
