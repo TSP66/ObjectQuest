7777dcb555d2eddf3cfc8116e6135e6f3db8b87d
Jack Anderson <a1852156@adelaide.edu.au>
2024-10-14 15:33:45 +1030

    Jack

--- AquaticAnimal.cpp
+++ AquaticAnimal.cpp
@@ -4,18 +4,18 @@ AquaticAnimal::AquaticAnimal(int id, std::string name, int lifespan) : Animal(id
 
 }
 
-Death AquaticAnimal::timestep(){ //Returns true if has died
+Death AquaticAnimal::timestep(){//Returns true if has died.
 
-    AquaticAnimal::age += 7.0/52.0; //Ages one week at a time
+    AquaticAnimal::age += 7.0/52.0;//Ages one week at a time.
     AquaticAnimal::hunger += 1.0;
     AquaticAnimal::happiness -= AquaticAnimal::hunger;
 
-    //Animals die if happiness reaches zero
+//Animals die if happiness reaches zero.
     if (AquaticAnimal::happiness <= 0){
         return STARVATION;
     }
     
-    //Animals randomly die as they get older
+//Animals randomly die as they get older.
     if (rand() < (RAND_MAX*AquaticAnimal::age)/(2.0*AquaticAnimal::get_lifespan())){
         if (AquaticAnimal::age >= (3*AquaticAnimal::get_lifespan())/4){
             return OLD_AGE;
